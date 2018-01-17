23e38b6a433e1b3f19498b2ee3e899be84d1bd1d
Saugat Dhungel <saugat.dhungel@gmail.com>
2018-01-17 15:01:39 -0500

    iterating over object properties drill

--- gist-object-drill.js
+++ gist-object-drill.js
@@ -1,13 +1,25 @@
 'use strict';
 
-const loaf = {
-  flour: 300,
-  water: 210,
-  hydration: function () {
-    const hydroCalc = (this.water / this. flour) * 100;
-    return hydroCalc;
-  }
+// const loaf = {
+//   flour: 300,
+//   water: 210,
+//   hydration: function () {
+//     const hydroCalc = (this.water / this. flour) * 100;
+//     return hydroCalc;
+//   }
+// }
+
+// console.log(`${loaf.flour} ${loaf.water}`);
+// console.log(loaf.hydration());
+
+const obj = {
+  foo: "Hello",
+  bar: "Ola",
+  fum: "Aloha",
+  quux: "Bonjour",
+  spam: "Hallo"
 }
 
-console.log(`${loaf.flour} ${loaf.water}`);
-console.log(loaf.hydration());
+for (let key in obj){
+  console.log(`${key}:${obj[key]}`)
+}
