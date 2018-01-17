0c882f6814e26a08f61aee21ac3cf060abbf4fbb
Adrian Ross <adriantoddross@gmail.com>
2018-01-17 17:02:15 -0500

    finished half of drill 7

--- gist-object-drill.js
+++ gist-object-drill.js
@@ -54,28 +54,89 @@
 
 
 
-function decode (word){
+// function decode (word){
 
-const cipher = {
-  a: 2,
-  b: 3,
-  c: 4,
-  d: 5
-}
+//   const cipher = {
+//     a: 2,
+//     b: 3,
+//     c: 4,
+//     d: 5
+//   };
 
-const splitWords = word.split(" ")
-const splitArray = splitWords.map ( word => {
-  let offset;
-  if(cipher[word[0]]){
-    offset = cipher[word[0]] - 1
-  }
-  return offset ? word[offset] : " ";
- })
-// else {
-//   console.log(" ")
+//   const splitWords = word.split(' ');
+//   const splitArray = splitWords.map ( word => {
+//     let offset;
+//     if(cipher[word[0]]){
+//       offset = cipher[word[0]] - 1;
+//     }
+//     return offset ? word[offset] : ' ';
+//   });
+//   // else {
+//   //   console.log(" ")
+//   // }
+
+//   return splitArray.join('');
 // }
 
-return splitArray.join("");
+// console.log(decode('craft block argon meter bells brown croon droop'));
+
+// DRILL 7
+
+// const lotrCharacters = {
+
+//   { name: 'Gandalf The White',
+//     nickname: 'Gandalf',
+//     race: 'Wizard',
+//     origin:'Middle Earth',
+//     attack: 10,
+//     defense: 6,
+//   }
+//   { name: 'Bilbo Baggins',
+//     nickname: 'Bilbo',
+//     race: 'Hobbit',
+//     origin:'The Shire',
+//     attack: 2,
+//     defense: 1,
+//   }
+//   { name: 'Frodo Baggins,
+//     nickname: 'Frodo',
+//     race: 'Hobbit',
+//     origin:'The Shire',
+//     attack: 6,
+//     defense: 8,
+//   }
+//   { name: 'Aragorn son of Arathorn',
+//     nickname: 'Aragorn',
+//     race: 'Man',
+//     origin:'Dunnedain',
+//     attack: 6,
+//     defense: 8,
+//   }
+//   { name: 'Legolas',
+//     nickname: 'Legolas',
+//     race: 'Elf',
+//     origin:'Woodland Realm',
+//     attack: 8,
+//     defense: 5,
+//   }
+  
+// }
+
+
+
+function createCharacter (name, nickname, race, origin, attack, defense, describe) {
+  return {
+    name, nickname, race, origin, attack, defense,
+    describe() {
+      console.log(`${name} is a ${race} from ${origin}.`);
+    },
+    evaluateFight(character) {
+      let attacker = 0;
+      let defender = 0;
+      if (defense > attack) {
+        attacker =
+      } if () {}
+    },
+  };
 }
 
-console.log(decode("craft block argon meter bells brown croon droop"));
